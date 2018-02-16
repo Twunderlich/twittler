baee78455119c1ab13e31af8baea211d3c680501
twunderlich <wunderlich.travis@gmail.com>
2018-02-15 21:24:33 -0600

    startIndex isn't updating correctly, add old twits with new twits

--- javascript.js
+++ javascript.js
@@ -1,105 +1,123 @@
-      $(document).ready(function(){
-        var $body = $('body');
-        var startIndex = 0;
-        var currentTime = new Date();
+$(document).ready(function(){
+  var $body = $('body');
+  var startIndex = 0;
+  var currentUser = $('.currentUser').text();
 
-        // var usersObjects = [];
-        // users.forEach(function(val, i) {
-        // usersObjects.push({name: val, startIndex: 0});
-        // });
+  /* create Twit elements and add them to feed */
+    // ToDo - want to reverse the feed so newest elements always on top
+      // possible solution:
+        // $('.content').first('.twit').prepend($twit);
+        // $twit.appendTo('.content');
 
-        // var counter = 0;
-        // var updateTime = function(timeStamp) {
-        //   counter += 1
-        //   setTimeout(updateTime, 60000);
-        // }
-        // updateTime();
+  if (currentUser === 'Me') {
+    getAllTwits(startIndex);
+  } else {
+    getUserTwits(currentUser, startIndex);
+  }
+
+  function getAllTwits(startPoint) {
+    var $content = $('.content');
+    var allTwits = streams.home.slice(startPoint);
+    allTwits.forEach(function(ele, i) {
+      var twit = streams.home[i];
+      var $twit = $('<article class="twit ' + twit.user + '" />');
+
+      $twit.html('<a href="#">@' + twit.user + '</a>:<blockquote> ' +     twit.message + '</blockquote><time>' + twit.created_at + '</time>');
+      $content.append($twit);
+    });
+
+    startIndex = streams.home.length - 1;
+  }
+
+  function getUserTwits(person, startPoint) {
+    var $content = $('.content');
+    var userTwits = streams.users[person].slice(startPoint);
+    userTwits.forEach(function(ele, i) {
+      var twit = ele
+      var $twit = $('<article class="twit ' + twit.user + '" />');
+
+      $twit.html('<a href="#">@' + twit.user + '</a>:<blockquote> ' +     twit.message + '</blockquote><time>' + twit.created_at + '</time>');
+      $content.append($twit);
+    });
+    startIndex = streams.users[person].length - 1;
+  };
+
+  function checkForNewTwits(){
+    if ((streams.home.length - 1) - startIndex > 10) {
+      $('#update').slideDown();
+    }
+    setTimeout(checkForNewTwits, 500);
+  };
+
+  checkForNewTwits();
+
+  $('#update').on('click', function() {
+      alert(startIndex);
+      getAllTwits(startIndex);
+      startIndex = streams.home.length - 1;
+    $('#update').slideUp();
+  });
+
+});
 
 
-        /* populate following list */
-
-        function getFollowees() {
-          var index = users.length - 1;
-          while (index >= 0) {
-            var followee = users[index];
-            var $followee = $('<div class="followee"></div>');
-            $followee.text(users[index]);
-            $followee.appendTo('.following');
-            index -= 1
-            $followee.click(filterByUser)
-          }
-        };
-
-        function filterByUser() {
-          var $name = $(this).text();
-          // alert('.twit .' + $name);
-          $('.twit').hide();
-          $('.' + $name).show();
-        }
 
 
-        getFollowees();
 
-        /* create Twit elements and add them to feed */
-          // ToDo - want to reverse the feed so newest elements always on top
 
-        function getAllTwits(startPoint) {
-          var $content = $('.content');
-          var newTwits = streams.home.slice(startPoint);
-          startIndex = streams.home.length - 1;
-          newTwits.forEach(function(ele, i) {
-            var twit = streams.home[i];
-            var $twit = $('<article class="twit ' + twit.user + '" />');
 
-            $twit.html('<a href="#">@' + twit.user + '</a>:<blockquote> ' +     twit.message + '</blockquote><time>' + twit.created_at + '</time>');
-            $content.append($twit);
-          });
-        }
 
-        // var getAllTwits = function(startPoint) {
-        //   var index = streams.home.length - 1;
-        //   var tempStartIndex = streams.home.length
-        //   while (index >= startPoint) {
-        //     var twit = streams.home[index];
-        //     var $twit = $('<div class="twit"></div>');
-        //     $twit.text('@' + twit.user + ': ' + twit.message + ' - ' + twit.created_at);
-        //     $twit.appendTo($('.content'));
-        //     index -= 1;
-        //   }
-        //   startIndex = tempStartIndex;
-        // };
 
-        getAllTwits(startIndex);
 
-        /* shows 'show new twits' button */
-        var checkForNewTwits = function(){
-          if ((streams.home.length - 1) - startIndex > 10) {
-            $('#update').slideDown();
-          }
-          setTimeout(checkForNewTwits, 500);
-        };
 
-        $('#update').on('click', function() {
-          if (streams.home.length - 1 > startIndex) {
-            getAllTwits(startIndex);
-          }
-          $('#update').slideUp();
-        });
 
-        checkForNewTwits();
 
-        /* get twits per indiviual (not currently used)*/
-        var getTwits = function(person) {
-          var twitter = person;
-          var index = streams.users[twitter].length - 1;
-          while (index >= 0) {
-            var twit = streams.users[twitter][index];
-            var $twit = $('<div class= "twit"></div>')
-            $twit.text('@' + twit.user + ': ' + twit.message);
-            $('.content').first('.twit').prepend($twit);
-            $twit.appendTo('.content');
-            index -= 1;
-          }
-        };
 
-      });
+  /* populate following list */
+      // not using
+  function getFollowees() {
+    var index = users.length - 1;
+    while (index >= 0) {
+      var followee = users[index];
+      var $followee = $('<div class="followee"></div>');
+      $followee.text(users[index]);
+      $followee.appendTo('.following');
+      index -= 1
+      $followee.click(filterByUser)
+    }
+  };
+
+  // getFollowees();
+
+  /* filter feed */
+    // not using
+  $('.dropdown').on('click', function() {
+    $('.dropDownList').toggle();
+  });
+
+  function populateFilter() {
+    var allFollowees = users;
+    allFollowees.forEach(function(ele, i) {
+      var $followee = $('<div class="followee"></div>');
+      $followee.text(ele);
+      $followee.appendTo('.dropDownList');
+      $followee.click(filterByUser)
+    });
+  }
+
+  $('.all').on('click', function (){
+    filterByUser();
+  });
+
+  // populateFilter();
+
+  function filterByUser() {
+    var $name = $(this).text();
+    if ($name === '') {
+      $('.twit').show();
+    } else {
+      $('.twit').hide();
+      $('.' + $name).show();
+    }
+  }
+
